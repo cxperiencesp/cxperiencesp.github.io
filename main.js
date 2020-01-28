ace339f36c20950e609ebc674cc9932342c5e8cd
juliacomj <julialcantara.ra@gmail.com>
2020-01-28 17:07:01 -0300

    css changes

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 var palestrantes = 'palestrantes';
+var programacao = 'programacao';
+var ingressos = "ingressos";
+var parceiros = "parceiros";
 
-document.body.innerHTML = palestrantes.anchor('palestrantes')
+var sectionPalestrantes = document.getElementsById("palestrantes");
+var sectionProgramacao = document.getElementById("programacao");
+var sectionIngressos = document.getElementById("ingressos");
+
+
+sectionPalestrantes.innerHTML = palestrantes.anchor('palestrantes');
+sectionProgramacao.innerHTML = programacao.anchor('programacao');
+sectionIngressos.innerHTML = ingressos.anchor('ingressos')
+
